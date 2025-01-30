40d73430176dd3f87aba4b0bb7c459bc61051ae2
Aryan Arora <aryan.arora@canctb.ca>
2025-01-30 01:22:29 -0800

    Auto commit

--- jan28.py
+++ jan28.py
@@ -13,3 +13,67 @@
 # GET /api/products/{company_name}
 # GET /api/products/{category}
 # GET /api/products/{company_name}/{category} != # GET /api/products/{category}/{company_name}
+
+# ALL THE ORDER
+# GET /orders
+# POST /orders
+
+# SPECIFIC
+# GET /orders/{order_id}
+
+# POST /orders 
+# REQ BODY: { "name": "Product Name", "quantity": 5, "address": "123 Main St" }
+# RES:      { "id": 123, "name": "Product Name", "quantity": 5, "address": "123 Main St" }
+
+# DELETE /orders/{order_id}
+# DELETE /orders
+
+
+# Entity: ORDER
+
+# CREATE: POST
+# READ:   GET
+# UPDATE: PATCH, PUT
+# DELETE: DELETE
+
+# STATUS CODES
+# 1XX
+# 2XX SUCCESS
+# 3XX
+# 4XX CLIENT ERROR
+# 5XX SERVER ERROR
+
+
+# /posts/{post_id}/comments EQUIVALENT
+# /posts
+
+# /posts/comments EQUIVALENT
+# /reviews/comments
+# /product/comments
+
+# /comments/post EQUIVALENT
+# /comments/reviews
+# /comments/prouct
+
+# /comments?post_id=10 EQUIVALENT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# • GET /reviews/{id}/comments
+# • GET /posts/{id}/comments
+
+# • GET /comments
+# • GET /comments?reviewId={id}
+
+
